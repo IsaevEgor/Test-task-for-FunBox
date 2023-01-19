9df552ab09d4d472ee72a5f02fb88489df669578
IsaevEgor <isaevegor.mail@gmail.com>
2023-01-19 11:07:39 +0300

    start project

---
+++ src/component/container/Container.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import st from "./style.module.css";
+	
+export const Container = () => {
+		return (
+				<div className={st.container}>
+						knlholnkl
+				</div>
+		);
+};
